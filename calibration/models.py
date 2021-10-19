66b8741efc07ab3c1a72805d45cfc95741618282
Apostolos Papas <apostolisparga@gmail.com>
2021-10-19 16:49:17 +0100

    Added specifics.py/default tds values

--- calibration/models.py
+++ calibration/models.py
@@ -1,111 +1,8 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
 from django.core.validators import MaxValueValidator, MinValueValidator
+from .specifics import *
 
-ACIDITY = (
-    ("Delicate","Delicate"),
-    ("Mild","Mild"),
-    ("Nippy","Nippy"),
-    ("Piquant","Piquant"),
-    ("Sweet","Sweet"),
-    ('Tangy', 'Tangy'),
-    ('Tart', 'Tart'),
-    ('Lemon','Lemon'),
-    ('Vibrant', 'Vibrant'),
-    ('Grassy', 'Grassy'),
-    ('Hard', 'Hard'),
-    ('Soft', 'Soft'),
-    ('Sour', 'Sour'),
-    ('Winey', 'Winey'),
-    ('Grapefruit', 'Grapefruit'), 
-)
-
-AROMAS = (
-    ('Berry', 'Berry'),
-    ('Floral', 'Floral'),
-    ('Fruity', 'Fruity'),
-    ('Caramel', 'Caramel'),
-    ('Resinous', 'Resinous'),
-    ('Lemon', 'Lemon'),
-    ('Grapefruit', 'Grapefruit'),
-    ('Chocolate', 'Chocolate'),
-    ('Spicy', 'Spicy'),
-    ('Earthy', 'Earthy'),
-    ('Nutty', 'Nutty'),
-    ('Malty', 'Malty'),
-    ('Carbonic', 'Carbonic'),
-    ('Pepper', 'Pepper'),
-    ('Vanilla', 'Vanilla'),
-    ('Hidy', 'Hidy'),
-    ('Musty', 'Musty'),
-    ('Leather', 'Leather'),
-    ('Butter', 'Butter'),
-    ('Toast', 'Toast'),
-    ('Smoke', 'Smoke'),
-)
-
-FLAVOUR = (
-    ('Sweet', 'Sweet'),
-    ('Sour', 'Sour'),
-    ('Salt', 'Salt'),
-    ('Delicate', 'Delicate'),
-    ('Rich', 'Rich'),
-    ('Intense', 'Intense'),
-    ('Pungent', 'Pungent'),
-    ('Musty', 'Musty'),
-    ('Pest-Crop', 'Pest-Crop'),
-    ('Woody', 'Woody'),
-    ('Flat', 'Flat'),
-    ('Greenish', 'Greenish'),
-    ('Fruity', 'Fruity'),
-    ('Balanced', 'Balanced'),
-    ('Exotic', 'Exotic'),
-    ('Chocolate', 'Chocolate'),
-    ('Spicy', 'Spicy'),
-    ('Nutty', 'Nutty'),
-)
-
-TEXTURE = (
-    ('Buttery','Buttery'),
-    ('Creamy','Creamy'),
-    ('Smooth','Smooth'),
-    ('Rich','Rich'),
-    ('Velvety','Velvety'),
-    ('Watery','Watery'),
-    ('Oily','Oily'),
-    ('Dry','Dry'),
-    ('Chalky','Chalky'),
-    ('Gritty','Gritty'),
-    ('Rough','Rough'),
-    ('Astringent','Astringent'),
-    ('Metallic','Metallic'),
-)
-
-FINISH = (
-    ('Short','Short'),
-    ('Dry','Dry'),
-    ('Bitter','Bitter'),
-    ('Sour','Sour'),
-    ('Harsh','Harsh'),
-    ('Dirty','Dirty'),
-    ('Clean','Clean'),
-    ('Floral','Floral'),
-    ('Fruity','Fruity'),
-    ('Lingering','Lingering'),
-    ('Resonant','Resonant'),
-)
-
-GENERAL = (
-    ("Low", "Low"),
-    ("Low-Medium", "Low-Medium"),
-    ("Medium", "Medium"),
-    ("Medium-High", "Medium-High"),
-    ("High", "High"),
-)
-METHOD = (
-    ("Batch", "Batch"),
-    ("v60", "v60"),
-)
 
 # Create your models here.
 class User(AbstractUser):
@@ -142,7 +39,7 @@ class Espresso(models.Model):
     coffee_name = models.ForeignKey(Coffee, on_delete=models.CASCADE, related_name="coffee_name", limit_choices_to={"roast_profile": "Espresso"})
     timestamp = models.DateTimeField(auto_now_add=True)
     water_temp = models.DecimalField(max_digits=3, decimal_places=1)
-    water_tds = models.DecimalField(max_digits=3, decimal_places=2)
+    water_tds = models.DecimalField(max_digits=3, decimal_places=2, default=1.03)
     batch_number = models.CharField(max_length=8)
     dry_weight = models.DecimalField(max_digits=3, decimal_places=1, default=18.0)
     wet_weight = models.IntegerField(default=36)
@@ -171,7 +68,7 @@ class Filter(models.Model):
     method = models.CharField(max_length=5, choices=METHOD, default="Batch")
     timestamp = models.DateTimeField(auto_now_add=True)
     water_temperature = models.DecimalField(max_digits=3, decimal_places=1)
-    water_tds = models.DecimalField(max_digits=3, decimal_places=2)
+    water_tds = models.DecimalField(max_digits=3, decimal_places=2, default=1.03)
     grind_size = models.DecimalField(max_digits=3, decimal_places=1)
     recipe = models.TextField()
     acidity_score = models.FloatField(validators=[MinValueValidator(6), MaxValueValidator(10)])
---
+++ calibration/specifics.py
@@ -0,0 +1,106 @@
+""" Contains tuples of specific form field choices """
+
+ACIDITY = (
+    ("Delicate","Delicate"),
+    ("Mild","Mild"),
+    ("Nippy","Nippy"),
+    ("Piquant","Piquant"),
+    ("Sweet","Sweet"),
+    ('Tangy', 'Tangy'),
+    ('Tart', 'Tart'),
+    ('Lemon','Lemon'),
+    ('Vibrant', 'Vibrant'),
+    ('Grassy', 'Grassy'),
+    ('Hard', 'Hard'),
+    ('Soft', 'Soft'),
+    ('Sour', 'Sour'),
+    ('Winey', 'Winey'),
+    ('Grapefruit', 'Grapefruit'), 
+)
+
+AROMAS = (
+    ('Berry', 'Berry'),
+    ('Floral', 'Floral'),
+    ('Fruity', 'Fruity'),
+    ('Caramel', 'Caramel'),
+    ('Resinous', 'Resinous'),
+    ('Lemon', 'Lemon'),
+    ('Grapefruit', 'Grapefruit'),
+    ('Chocolate', 'Chocolate'),
+    ('Spicy', 'Spicy'),
+    ('Earthy', 'Earthy'),
+    ('Nutty', 'Nutty'),
+    ('Malty', 'Malty'),
+    ('Carbonic', 'Carbonic'),
+    ('Pepper', 'Pepper'),
+    ('Vanilla', 'Vanilla'),
+    ('Hidy', 'Hidy'),
+    ('Musty', 'Musty'),
+    ('Leather', 'Leather'),
+    ('Butter', 'Butter'),
+    ('Toast', 'Toast'),
+    ('Smoke', 'Smoke'),
+)
+
+FLAVOUR = (
+    ('Sweet', 'Sweet'),
+    ('Sour', 'Sour'),
+    ('Salt', 'Salt'),
+    ('Delicate', 'Delicate'),
+    ('Rich', 'Rich'),
+    ('Intense', 'Intense'),
+    ('Pungent', 'Pungent'),
+    ('Musty', 'Musty'),
+    ('Pest-Crop', 'Pest-Crop'),
+    ('Woody', 'Woody'),
+    ('Flat', 'Flat'),
+    ('Greenish', 'Greenish'),
+    ('Fruity', 'Fruity'),
+    ('Balanced', 'Balanced'),
+    ('Exotic', 'Exotic'),
+    ('Chocolate', 'Chocolate'),
+    ('Spicy', 'Spicy'),
+    ('Nutty', 'Nutty'),
+)
+
+TEXTURE = (
+    ('Buttery','Buttery'),
+    ('Creamy','Creamy'),
+    ('Smooth','Smooth'),
+    ('Rich','Rich'),
+    ('Velvety','Velvety'),
+    ('Watery','Watery'),
+    ('Oily','Oily'),
+    ('Dry','Dry'),
+    ('Chalky','Chalky'),
+    ('Gritty','Gritty'),
+    ('Rough','Rough'),
+    ('Astringent','Astringent'),
+    ('Metallic','Metallic'),
+)
+
+FINISH = (
+    ('Short','Short'),
+    ('Dry','Dry'),
+    ('Bitter','Bitter'),
+    ('Sour','Sour'),
+    ('Harsh','Harsh'),
+    ('Dirty','Dirty'),
+    ('Clean','Clean'),
+    ('Floral','Floral'),
+    ('Fruity','Fruity'),
+    ('Lingering','Lingering'),
+    ('Resonant','Resonant'),
+)
+
+GENERAL = (
+    ("Low", "Low"),
+    ("Low-Medium", "Low-Medium"),
+    ("Medium", "Medium"),
+    ("Medium-High", "Medium-High"),
+    ("High", "High"),
+)
+METHOD = (
+    ("Batch", "Batch"),
+    ("v60", "v60"),
+)
